0c68ea183c097c58a001bb270283a2c12e23a7ad
deltagphys <cty.jgates@gmail.com>
2020-01-03 11:37:32 -0500

    allllll

--- src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/GenericUtils.java
@@ -11,66 +11,66 @@ public class GenericUtils<T> {
 
 
     public static <T extends Comparable> Iterable<? extends Iterable<T>> powerSet(Set<T> originalSet) {
-        Set<Set<T>> sets = new LinkedHashSet<>();
+        List<List<T>> sets = new ArrayList<>();
         ArrayList<T> items = new ArrayList<>(originalSet);
         if (originalSet.size() == 2) {
-            sets.add(originalSet);
+            sets.add(new ArrayList<>(originalSet));
 
-            Set<T> set1 = new LinkedHashSet<T>();
+            ArrayList<T> set1 = new ArrayList<T>();
             set1.add(items.get(0));
             sets.add(set1);
-            Set<T> set2 = new LinkedHashSet<T>();
+            ArrayList<T> set2 = new ArrayList<T>();
             set2.add(items.get(1));
             sets.add(set2);
 
-            sets.add(new LinkedHashSet<T>());
+            sets.add(new ArrayList<T>());
 
         } else if (originalSet.size() == 3) {
-            sets.add(originalSet);
+            sets.add(new ArrayList<>(originalSet));
 
-            Set<T> set4 = new LinkedHashSet<T>();
+            ArrayList<T> set4 = new ArrayList<T>();
             set4.add(items.get(0));
             set4.add(items.get(1));
             sets.add(set4);
 
-            Set<T> set5 = new LinkedHashSet<T>();
+            ArrayList<T> set5 = new ArrayList<T>();
             set5.add(items.get(0));
             set5.add(items.get(2));
             sets.add(set5);
 
-            Set<T> set1 = new LinkedHashSet<T>();
+            ArrayList<T> set1 = new ArrayList<T>();
             set1.add(items.get(0));
             sets.add(set1);
 
-            Set<T> set6 = new LinkedHashSet<T>();
+            ArrayList<T> set6 = new ArrayList<T>();
             set6.add(items.get(1));
             set6.add(items.get(0));
             sets.add(set6);
 
-            Set<T> set7 = new LinkedHashSet<T>();
+            ArrayList<T> set7 = new ArrayList<T>();
             set7.add(items.get(1));
             set7.add(items.get(2));
             sets.add(set7);
-//
-//            Set<T> set2 = new LinkedHashSet<T>();
-//            set2.add(items.get(1));
-//            sets.add(set2);
-//
-//            Set<T> set8 = new LinkedHashSet<T>();
-//            set8.add(items.get(2));
-//            set8.add(items.get(0));
-//            sets.add(set8);
-//
-//            Set<T> set9 = new LinkedHashSet<T>();
-//            set9.add(items.get(2));
-//            set9.add(items.get(1));
-//            sets.add(set9);
-//
-//            Set<T> set3 = new LinkedHashSet<T>();
-//            set2.add(items.get(2));
-//            sets.add(set3);
-//
-//            sets.add(new LinkedHashSet<T>());
+
+            ArrayList<T> set2 = new ArrayList<T>();
+            set2.add(items.get(1));
+            sets.add(set2);
+
+            ArrayList<T> set8 = new ArrayList<T>();
+            set8.add(items.get(2));
+            set8.add(items.get(0));
+            sets.add(set8);
+
+            ArrayList<T> set9 = new ArrayList<T>();
+            set9.add(items.get(2));
+            set9.add(items.get(1));
+            sets.add(set9);
+
+            ArrayList<T> set3 = new ArrayList<T>();
+            set3.add(items.get(2));
+            sets.add(set3);
+
+            sets.add(new ArrayList<T>());
 
         }
 
